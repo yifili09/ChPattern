6f23d3f994608e125a083010937a9da94fc8ac74
Entergate <frankhalltorp@sbykl-tabletp.(none)>
2012-02-24 15:40:22 +0100

    Initial Commit

---
+++ AbsFactory/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  AbsFactory
+//
+//  Created by Entergate on 2012-02-24.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ AbsFactory/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AbsFactory
+//
+//  Created by Entergate on 2012-02-24.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
