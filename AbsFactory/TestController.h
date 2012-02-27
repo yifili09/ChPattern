5c9cdb2c5f59e71da79667bb73e4caf467e1b46c
Yifili09 <yifili09@gmail.com>
2012-02-27 11:21:27 +0100

    ss

---
+++ AbsFactory/TestController.h
@@ -0,0 +1,13 @@
+//
+//  TestController.h
+//  AbsFactory
+//
+//  Created by Entergate on 2012-02-27.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestController : UITableViewController
+
+@end
